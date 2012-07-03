268ef0c1cb71576f75542dfc28824410c5fd780e
MURAOKA Taro <koron.kaoriya@gmail.com>
2012-07-03 23:49:09 +0900

    impl WiFi monitor

--- src/net/kaoriya/rslog/MainActivity.java
+++ src/net/kaoriya/rslog/MainActivity.java
@@ -1,16 +1,29 @@
 package net.kaoriya.rslog;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiInfo;
 
 public class MainActivity extends Activity
 {
     public static final String TAG = "rslog";
+    public static final String ACTION_LOG_STRENGTH = "LogStrength";
+
+    private PhoneStateListener phoneStateListener = null;
+    private PendingIntent pendingIntent = null;
+    private BroadcastReceiver broadcastReceiver = null;
+    private int lastRssi = 0;
 
     /** Called when the activity is first created. */
     @Override
@@ -21,18 +34,72 @@ public class MainActivity extends Activity
 
         TelephonyManager manager = (TelephonyManager)getSystemService(
                 Context.TELEPHONY_SERVICE);
-        manager.listen(new PhoneStateListener() {
+        this.phoneStateListener = new PhoneStateListener() {
             @Override
             public void onSignalStrengthsChanged(SignalStrength s)
             {
                 log(s);
             }
-        }, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+        };
+        manager.listen(this.phoneStateListener,
+                PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+
+        this.broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (ACTION_LOG_STRENGTH.equals(intent.getAction())) {
+                    logWifiStrength();
+                }
+            }
+        };
+        registerReceiver(this.broadcastReceiver,
+                new IntentFilter(ACTION_LOG_STRENGTH));
+
+        AlarmManager alarm = (AlarmManager)getSystemService(
+                Context.ALARM_SERVICE);
+        this.pendingIntent = PendingIntent.getBroadcast(this, 0,
+                new Intent(ACTION_LOG_STRENGTH), 0);
+        alarm.setRepeating(AlarmManager.RTC, 1000, 3000, this.pendingIntent);
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        if (this.pendingIntent != null) {
+            AlarmManager alarm = (AlarmManager)getSystemService(
+                    Context.ALARM_SERVICE);
+            alarm.cancel(this.pendingIntent);
+            this.pendingIntent = null;
+        }
+        if (this.broadcastReceiver != null) {
+            unregisterReceiver(this.broadcastReceiver);
+            this.broadcastReceiver = null;
+        }
+        if (this.phoneStateListener != null) {
+            TelephonyManager manager = (TelephonyManager)getSystemService(
+                    Context.TELEPHONY_SERVICE);
+            manager.listen(this.phoneStateListener,
+                    PhoneStateListener.LISTEN_NONE);
+            this.phoneStateListener = null;
+        }
     }
 
     private void log(SignalStrength signalStrength)
     {
         Log.d(TAG, "Strength(3G)=" + signalStrength.toString());
+    }
+
+    private void logWifiStrength()
+    {
+        WifiManager manager = (WifiManager)getSystemService(
+                Context.WIFI_SERVICE);
+        WifiInfo info = manager.getConnectionInfo();
+        int curr = info.getRssi();
+        if (curr != this.lastRssi) {
+            Log.d(TAG, "Strength(WiFi)=" + curr);
+            this.lastRssi = curr;
+        }
     }
 
 }
